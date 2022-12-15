3be1ad1d4b8a788beb47028d1cd660205e961bcc
AlvarezMariaBelen <beluchis1991@gmail.com>
2022-12-15 03:16:37 -0300

    pagina prueba de productos

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {SafeAreaView, StyleSheet, Switch, Text, View } from "react-native";
+import { useColorScheme } from "nativewind";
+import ListaProductos from "./componentes/ListaProductos";
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black pt-10">
+      <View className="flex-row w-full gap-5">
+        <Text className="text-2xl font-bold dark:text-white">
+          Nueva colección
+        </Text>
+        <Switch
+          value={colorScheme === "dark"}
+          onChange={toggleColorScheme}
+        ></Switch>
+      </View>
+      <ListaProductos></ListaProductos>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ componentes/ProductoCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useColorScheme } from "nativewind";
+
+export default function ProductoCard({
+  nombre,
+  foto1,
+  talle,
+  descripcion,
+  precio,
+}) {
+  const [count, setCount] = React.useState(1);
+  const { colorScheme } = useColorScheme();
+  return (
+    <View className="w-full bg-gray-400 dark:bg-gray-50/10 rounded-3xl p-5 my-5">
+        <Text className='text-lg font-semibold dark:text-white/70'>{nombre}</Text>
+      <View>
+        <Image
+          source={{ uri: foto1 }}
+          className="w-full h-72"
+          style={{ resizeMode: "contain" }}
+        ></Image>
+      </View>
+      <View className='flex-row justify-between my-3'>
+      <View className='flex-row items-center gap-3'>
+        <AntDesign name='minuscircle' size={24} color={colorScheme === 'light' ? 'black' : 'white'}
+        onPress={()=>setCount(count-1)}></AntDesign>
+        <Text className='text-xl dark:text-white'>{count}</Text>
+        <AntDesign name='pluscircle' size={24} color={colorScheme === 'light' ? 'black' : 'white'}
+        onPress={()=>setCount(count+1)}></AntDesign>
+        </View>
+        <Text className='text-2xl font-extrabold dark:text-white'>${precio * count}</Text>
+        </View>
+   <View className='mt-5'>     
+      <Text className='text-sm text-black/60 dark:text-white/70' numberOfLines={2}>{descripcion}</Text>
+      <Text className='text-sm text-black/60 dark:text-white'>Talle: {talle}</Text>
+      <TouchableOpacity className='flex-row justify-center w-10/12 self-center mt-5 bg-black dark:bg-white p-3 rounded-full'>
+        <Text className='text-white dark:text-black font-bold'>Añadir al carro</Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
