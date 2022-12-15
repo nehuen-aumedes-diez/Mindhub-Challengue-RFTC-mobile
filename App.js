685a93c3bb4682a8a09b96e73815f111f60aaaf3
nehuen-aumedes-diez <nehuen.aumedes@gmail.com>
2022-12-15 15:12:51 -0300

    drawer ya finalizado beluluuu

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { SafeAreaView, StyleSheet, Switch, Text, View } from "react-native";
 import { useColorScheme } from "nativewind";
 import ListaProductos from "./screens/ListaProductos";
 import Home from './screens/Home'
+import { NavigationContainer } from '@react-navigation/native';
+import Drawer from './navegacion/Drawer';
+
 
 export default function App() {
   return (
-    <SafeAreaView className="mt-10">
-      <Home></Home>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer >
+      <Drawer/>
+    </NavigationContainer>
   );
 }
