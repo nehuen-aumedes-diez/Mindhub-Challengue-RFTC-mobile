3e5b0d5bb7b034b6ff404f3bec66bb65afca5b06
ignaarguello <igna.arguello2110@hotmail.com>
2022-12-21 17:02:45 -0300

    Mejoras en Screen Tienda

--- componentes/CardHome.js
+++ componentes/CardHome.js
@@ -53,7 +53,7 @@ export default function CardHome() {
     return (
         <ScrollView className='h-full bg-black'>
             <Animated.Text style={{ opacity: fadeAnim }} className="text-yellow-500 font-extralight text-4xl text-center mt-10 tracking-widest"> - Retro Football - </Animated.Text>
-            <Text className="text-white font-thin text-lg text-left ml-24 tracking-wider"> Team Clothing</Text>
+            <Text className="text-white font-thin text-lg text-center tracking-wider"> Team Clothing</Text>
             <Animated.View style={{ opacity: fadeAnim }} className="w-full mt-5 align-center p-2">
                 <Image source={imagesBanner[counter]} resizeMode="cover" className="bg-red w-full h-screen" />
             </Animated.View>
