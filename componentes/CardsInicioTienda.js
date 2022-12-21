9b6d8cb5c787ccfdffb72569ae863276db23b2af
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2022-12-20 21:00:31 -0300

    nuevas acciones sin gorras

--- componentes/CardsInicioTienda.js
+++ componentes/CardsInicioTienda.js
@@ -13,14 +13,10 @@ export default function CardsInicioTienda() {
     const irARemeHombre = () => {
         navigation.navigate('Remeras de Hombres')
     }
-    const irAGorras = () => {
-        navigation.navigate('Gorras')
-    }
 
     const imagenBuzo = 'https://cdn.shopify.com/s/files/1/0015/5841/3357/collections/MODEL-HOODIE-DIABLO_720x.png?v=1571518856'
     const imagenRemeMujer = 'https://cdn.shopify.com/s/files/1/0015/5841/3357/collections/WOMEN-SQUARE-SOL_720x.png?v=1603886812'
     const imagenRemeHombre = 'https://cdn.shopify.com/s/files/1/0015/5841/3357/collections/MODEL-SOL-ALBI_720x.png?v=1571493500'
-    const imagenGorra = 'https://cdn.shopify.com/s/files/1/0015/5841/3357/collections/IMG_0121_720x.jpg?v=1654540849'
 
 return (
     <>
@@ -70,22 +66,6 @@ return (
             <Text className='text-lg text-black/60 dark:text-white/70 text-white'>Camisetas deportivas con corte slim fit, con bordados de alta calidad y diseños exclusivos.</Text>
             <TouchableOpacity className='flex-row justify-center w-10/12 self-center mt-5 bg-black dark:bg-white p-3 rounded-full'>
                 <Text className='text-white text-base dark:text-black font-bold' onPress={irARemeHombre}>Ir a esta sección</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-    <View className="w-full bg-red-900 rounded-3xl p-5 my-5">
-        <Text className='text-4xl text-center text-[#EEA904] font-semibold dark:text-white/70'>GORRAS</Text>
-        <View>
-            <Image
-                source={{ uri: imagenGorra }}
-                className="w-full h-72"
-                style={{ resizeMode: "contain", borderRadius: 25 }}
-            ></Image>
-        </View>
-        <View className='mt-5'>     
-            <Text className='text-lg text-black/60 dark:text-white/70 text-white'>Gorras con el escudo bordado en alta calidad, talles ajustables.</Text>
-            <TouchableOpacity className='flex-row justify-center w-10/12 self-center mt-5 bg-black dark:bg-white p-3 rounded-full'>
-                <Text className='text-white text-base dark:text-black font-bold' onPress={irAGorras}>Ir a esta sección</Text>
             </TouchableOpacity>
         </View>
     </View>
--- redux/actions/remeraFAction.js
+++ redux/actions/remeraFAction.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "../../api/url";
 
 const getRemeraF = createAsyncThunk("getRemeraF", async () => {
   try {
-    const res = await axios.get(`${BASE_URL}/remeraF/`);
+    const res = await axios.get(`${BASE_URL}/productos?tipo=remeraF`);
 
     return res.data.res;
 
@@ -18,7 +18,7 @@ const getRemeraF = createAsyncThunk("getRemeraF", async () => {
 const getOneRemeraFId = createAsyncThunk(
   "getOneRemeraFId",
   async (_id) => {
-    let url = ` ${BASE_URL}/remeraF/?_id=${_id}`;
+    let url = ` ${BASE_URL}/productos?tipo=remeraF&_id=${_id}`;
     try {
       const res = await axios.get(url);
       //console.log(res.data.res);
@@ -53,7 +53,7 @@ const deleteRemeraF = createAsyncThunk("deleteRemeraF", async ({remeraFId}) => {
 });
 
 const filtrarRemerasF = createAsyncThunk('filtrarRemerasF', async(nombre) => {
-  let url = ` ${BASE_URL}/remeraF?nombre=${nombre}`
+  let url = ` ${BASE_URL}/productos?tipo=remeraF&nombre=${nombre}`
   try{
       const res = await axios.get(url)
       //console.log("res", res);
