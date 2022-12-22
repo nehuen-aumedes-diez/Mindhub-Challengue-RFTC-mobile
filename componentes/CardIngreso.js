ae5389425d68a66a045fbe66e29c72d674a108c0
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-22 20:36:02 -0300

    se da funcionalidad al ingreso

--- componentes/CardIngreso.js
+++ componentes/CardIngreso.js
@@ -1,30 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { View, Text, TouchableOpacity, TextInput, ImageBackground, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch } from "react-redux";
+import userActions from '../redux/actions/userAction';
 
 export default function CardIngreso(){
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-   ;
-   let navigation = useNavigation()
-   const butRegis = () => {
-       navigation.navigate('Registro')
+    let dispatch = useDispatch()
+
+    let navigation = useNavigation()
+    const butRegis = () => {
+        navigation.navigate('Registro')
    }
-    async function signIn(event){
-        event.preventDefault();
-        let data = {
-            email,
-            password
-        }
-        AsyncStorage.setItem('data', JSON.stringify(data))
-        .catch(error=>console.error(error))
+  let [data,setData] = useState({
+      email: "",
+      password: ""
+  })
+  
+  console.log(data,1);
+
+  const handleText = (value, fieldName) => {
+    setData({ ...data, [fieldName]: value });
+  };
+
+
+    async function SignIn(e){
+   
+      try{
+        let res = await dispatch(userActions.signIn(data))
+        // console.log(4,res)
+        if (res.payload.success) {
+        //   swal({
+        //     title: "Bienvenido "+ res.payload.response.user.name,
+        //     // text:  "You are logged!!",
+        //     icon: "success",
+        //     timer: "3000"
+        // })
+          // redirect()
+          alert("Bienvenido "+ res.payload.response.user.name)
+          navigation.navigate('Inicio')
+          
+       }
+       else {
+        alert('usuario incorrecto')
+       }
+
+      } catch(error){
+        console.log(error,11);
+      }
     }
-    useEffect(()=>{
-        AsyncStorage.setItem('data')
-        .then(data=>console.log(data))
-        .catch(error=>console.log(error))
-    },[]);
+
     const image = {
         uri: "https://st.depositphotos.com/1051862/3469/i/600/depositphotos_34699129-stock-photo-abstract-red-gradient-background.jpg",
       };
@@ -42,17 +66,19 @@ export default function CardIngreso(){
           <TextInput
             className="text-center border p-2 rounded-full my-2 bg-white"
             placeholder="Correo"
-            onChangeText={setEmail}
-            value={email}
+            onChangeText={(item)=>{handleText(item,'email')}}
+            
           ></TextInput>
           <TextInput
             className="text-center border p-2 rounded-full my-2 mb-9 bg-white"
             placeholder="Contraseña"
-            onChangeText={setPassword}
-            value={password}
+            password={true}
+            secureTextEntry={true}
+            onChangeText={(item)=>{handleText(item,'password')}}
+           
           ></TextInput>
-          <TouchableOpacity className="flex-row justify-center w-8/12 self-center mt-5 bg-black dark:bg-white p-3 rounded-full border border-orange-300">
-            <Text onPress={() => navigation.navigate('Home') && signIn} className="text-center text-white bg-black font-bold">
+          <TouchableOpacity onPress={SignIn} className="flex-row justify-center w-8/12 self-center mt-5 bg-black dark:bg-white p-3 rounded-full border border-orange-300">
+            <Text  className="text-center text-white bg-black font-bold">
               Entrar
             </Text>
           </TouchableOpacity>
